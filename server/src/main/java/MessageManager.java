acf802b08a5609dfc980a6da04f94d49883a1481
Antoine FONTANEZ <126561427+Akip2@users.noreply.github.com>
2026-05-15 22:12:13 +0200

    refactoring creating MessageParser class

--- server/src/main/java/MessageManager.java
+++ server/src/main/java/MessageManager.java
@@ -1,17 +1,18 @@
 import bernard_flou.Fabricateur;
 import org.eclipse.paho.client.mqttv3.*;
 
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class MessageManager implements MqttCallback {
     private final Usine usine;
     private final MqttClient client;
+    private final MessageParser parser;
 
     public MessageManager(Usine usine, MqttClient client) {
         this.usine = usine;
         this.client = client;
+        this.parser = new MessageParser();
     }
 
     @Override
@@ -21,7 +22,7 @@ public class MessageManager implements MqttCallback {
         String payload = new String(message.getPayload());
 
         try {
-            Map<Fabricateur.TypeLunette, Integer> typeLunettes = parsePayload(payload);
+            Map<Fabricateur.TypeLunette, Integer> typeLunettes = parser.parsePayload(payload);
 
             // Si le parsing du payload n'a pas levé d'exception on valide la commande et on la traite
             publier(topic + "/validated", "");
@@ -56,60 +57,9 @@ public class MessageManager implements MqttCallback {
         }
     }
 
-    private Fabricateur.TypeLunette parseType(String type) throws IllegalArgumentException {
-        try {
-            return Fabricateur.TypeLunette.valueOf(type);
-        } catch (IllegalArgumentException e) {
-            throw new IllegalArgumentException("Type de lunette inconnu");
-        }
-    }
-
-    private boolean qteLegal(int qte) {
-        return (qte >= 0 && qte < 10);
-    }
-
-    private Map<Fabricateur.TypeLunette, Integer> parsePayload(String payload) throws IllegalArgumentException {
-        Map<Fabricateur.TypeLunette, Integer> res = new HashMap<>();
-        String[] commandes = payload.split(";");
-
-        int totalQte = 0;
-        for(String commande : commandes) {
-            String[] split = commande.split(":");
-
-            if(split.length != 2) {
-                throw new IllegalArgumentException("Format de commande invalide");
-            }
-
-            Fabricateur.TypeLunette type = this.parseType(split[0]);
-            int qte = Integer.parseInt(split[1]);
-
-            if(!qteLegal(qte)) {
-                throw new IllegalArgumentException("Quantité invalide, doit être comprise entre 0 (inclu) et 10 (exclu)");
-            }
-
-            totalQte += qte;
-            res.put(type, qte);
-        }
-
-        if(totalQte == 0) {
-            throw new IllegalArgumentException("Quantité totale invalide, doit être strictement supérieure à 0");
-        }
-
-        return res;
-    }
-
     private String traiterCommande(String orderId, Map<Fabricateur.TypeLunette, Integer> typesLunettes) {
         List<Fabricateur.Lunette> lunettes = this.usine.produire(typesLunettes);
 
-        return serializeLunettes(lunettes);
-    }
-
-    private String serializeLunettes(List<Fabricateur.Lunette> lunettes) {
-        StringBuilder res = new StringBuilder();
-        for(Fabricateur.Lunette lunette : lunettes) {
-            res.append(lunette.type).append(":").append(lunette.serial).append(";");
-        }
-
-        return res.toString();
+        return parser.serializeLunettes(lunettes);
     }
 }
